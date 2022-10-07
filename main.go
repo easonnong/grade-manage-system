ffb9e79f95aec0992fcf7e0180945e8be9af3d8d
easonnong <nongeason@gmail.com>
2022-10-07 14:44:21 +0800

    init database

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	//读取配置信息
+	InitConfig()
+}
+
+//从配置文件中读取
+func InitConfig() {
+	//获取当前工作目录
+	workDir, err := os.Getwd()
+	if err != nil {
+		panic("获取当前工作目录失败")
+	}
+	//设置读取文件的类型
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err = viper.ReadInConfig()
+	if err != nil {
+		panic("读取文件失败")
+	}
 }
